0c1a98f9175f8e2df277ed03ed57877169173748
Michael <apple@apples-MacBook-Pro.local>
2021-04-05 00:45:40 -0500

    Updates

---
+++ precache-manifest.0422ad9c84f30642da572511fe1f4463.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c348d713ffa328b77c5bd81220c5b769",
+    "url": "/netflix-video-clone/index.html"
+  },
+  {
+    "revision": "3df94886b409d3c7379c",
+    "url": "/netflix-video-clone/static/js/2.b6672955.chunk.js"
+  },
+  {
+    "revision": "2ec440bd74f96c350770",
+    "url": "/netflix-video-clone/static/js/main.39cc3b43.chunk.js"
+  },
+  {
+    "revision": "87815a89c9061342bd24",
+    "url": "/netflix-video-clone/static/js/runtime-main.184c4599.js"
+  }
+]);
